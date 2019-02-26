710568980a880c0c9218036d4e272545be0876c5
Cong Fandi <congfandi@gmail.com>
2019-02-26 16:38:24 +0700

    hasil sesore

--- lib/detail_s_o/detail_s_o_view.dart
+++ lib/detail_s_o/detail_s_o_view.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
 import './detail_s_o_view_model.dart';
-  
+
 class DetailSOView extends DetailSOViewModel {
-    
+  _body() {
+    return Container(
+      child: Column(
+        children: <Widget>[
+
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    
-    // Replace this with your build function
-    return Text('Just a placeholder');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Detail SO",
+          style: TextStyle(fontFamily: "Title", fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: _body(),
+    );
   }
 }
-
--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './home_view_model.dart';
 import 'package:forca_so/create_s_o/create_s_o.dart';
+import 'package:forca_so/detail_s_o/detail_s_o.dart';
 
 class HomeView extends HomeViewModel {
 
@@ -136,7 +137,9 @@ class HomeView extends HomeViewModel {
                   Container(
                     height: 30.0,
                     child: OutlineButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (c)=>DetailSO()));
+                      },
                       child: Text(
                         "Detail",
                         style: TextStyle(
@@ -148,10 +151,13 @@ class HomeView extends HomeViewModel {
                   ),
                   Row(
                     children: <Widget>[
-                      Container(
+
+                      status<3?Container(
                         height: 30.0,
                         child: OutlineButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            Navigator.push(context, MaterialPageRoute(builder: (c)=>CreateSO()));
+                          },
                           child: Text(
                             "Edit",
                             style: TextStyle(
@@ -160,7 +166,7 @@ class HomeView extends HomeViewModel {
                                 color: Colors.black),
                           ),
                         ),
-                      ),
+                      ):Container(),
                       Padding(
                         padding: EdgeInsets.only(right: 16.0),
                       ),

5374ba5f7c3dfab107506ceee9a68767d8f83261
glamvian <glamvian.hermawan@gmail.com>
2019-03-19 14:29:16 +0700

    issue-#11350 create mandatory field

--- lib/inventory_move/create_i_m/create_i_m_view.dart
+++ lib/inventory_move/create_i_m/create_i_m_view.dart
@@ -1,9 +1,204 @@
 import 'package:flutter/material.dart';
-import 'package:forca_so/inventory_move/create_i_m/create_i_m_view_model.dart';
+import 'package:forca_so/utils/string.dart';
+import './create_i_m_view_model.dart';
 
 class CreateIMView extends CreateIMViewModel{
-
-
+  _body(){
+    return Container(
+      margin: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Image.asset(
+            LOGO,
+            width: 110,
+          ),
+          Padding(padding: EdgeInsets.only(top: 20.0)),
+          Container(
+            margin: EdgeInsets.only(top: 16.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      width: MediaQuery.of(context).size.width / 2 - 20,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "Product",
+                            style: TextStyle(
+                                fontFamily: "Title",
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Container(
+                                width:
+                                MediaQuery.of(context).size.width / 2 - 70,
+                                child: Text(
+                                  'Select Product',
+                                  style: TextStyle(
+                                    fontFamily: "Title",
+                                    fontSize: 14.0,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                              IconButton(
+                                  icon: Icon(Icons.expand_more),
+                                  onPressed: () {}),
+                            ],
+                          ),
+                          Container(
+                            height: 1.0,
+                            color: Colors.grey[600],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width / 2 - 20,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "Locator From",
+                            style: TextStyle(
+                                fontFamily: "Title",
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Container(
+                                width:
+                                MediaQuery.of(context).size.width / 2 - 70,
+                                child: Text(
+                                  'Select Locator From',
+                                  style: TextStyle(
+                                    fontFamily: "Title",
+                                    fontSize: 14.0,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                              IconButton(
+                                  icon: Icon(Icons.expand_more),
+                                  onPressed: () {}),
+                            ],
+                          ),
+                          Container(
+                            height: 1.0,
+                            color: Colors.grey[600],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 16.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      width: MediaQuery.of(context).size.width / 2 - 20,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "Location To",
+                            style: TextStyle(
+                                fontFamily: "Title",
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Container(
+                                width:
+                                MediaQuery.of(context).size.width / 2 - 70,
+                                child: Text(
+                                  'Select Location To',
+                                  style: TextStyle(
+                                    fontFamily: "Title",
+                                    fontSize: 14.0,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                              IconButton(
+                                  icon: Icon(Icons.expand_more),
+                                  onPressed: () {}),
+                            ],
+                          ),
+                          Container(
+                            height: 1.0,
+                            color: Colors.grey[600],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width / 2 - 20,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "Movement Qty",
+                            style: TextStyle(
+                                fontFamily: "Title",
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Container(
+                                width:
+                                MediaQuery.of(context).size.width / 2 - 70,
+                                child: Text(
+                                  'Select Movement Qty',
+                                  style: TextStyle(
+                                    fontFamily: "Title",
+                                    fontSize: 14.0,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                              IconButton(
+                                  icon: Icon(Icons.expand_more),
+                                  onPressed: () {}),
+                            ],
+                          ),
+                          Container(
+                            height: 1.0,
+                            color: Colors.grey[600],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(top: 20.0)),
+        ],
+      ),
+    );
+  }
 
 
 
@@ -31,6 +226,7 @@ class CreateIMView extends CreateIMViewModel{
           )
         ],
       ),
+      body: _body(),
     );
   }
 

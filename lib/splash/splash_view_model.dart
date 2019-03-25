e35199bfe0600b5e4d5313bad10f45c69578cb26
glamvian <glamvian.hermawan@gmail.com>
2019-03-25 13:28:21 +0700

    issue-#11642 adding detail,edit and button card imLine

--- lib/splash/splash_view_model.dart
+++ lib/splash/splash_view_model.dart
@@ -3,12 +3,13 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import './splash.dart';
 import 'package:forca_so/home/home.dart';
+import 'package:forca_so/inventory_move/inventory_move.dart';
 
 abstract class SplashViewModel extends State<Splash> {
   // Add your state and logic here
   @override
   void initState() {
-    Timer(Duration(seconds: 3), ()=>Navigator.pushReplacement(context, MaterialPageRoute(builder: (c)=>Home())));
+    Timer(Duration(seconds: 3), ()=>Navigator.pushReplacement(context, MaterialPageRoute(builder: (c)=>InventoryMove())));
     super.initState();
   }
 }

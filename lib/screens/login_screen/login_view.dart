4e83ecb7b5f1fed883bb7a04c4d0e17c4fd1a0ec
Cong Fandi <congfandi@gmail.com>
2019-05-07 16:08:21 +0700

    making dinamic login screen

--- lib/screens/login_screen/login_view.dart
+++ lib/screens/login_screen/login_view.dart
@@ -4,6 +4,7 @@ import 'package:forca_so/utils/forca_assets.dart';
 import 'package:flutter_crashlytics/flutter_crashlytics.dart';
 
 class LoginView extends LoginViewModel {
+
   _body() {
     return Container(
       color: Colors.white,
@@ -17,51 +18,56 @@ class LoginView extends LoginViewModel {
             ),
             child: Container(
               padding: EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
+              height: 400.0,
+              child: ListView(
                 children: <Widget>[
-                  forcaLogo(width: 130),
-                  TextFormField(
-                    controller: username,
-                    decoration: InputDecoration(
-                        labelText: "Username", prefixIcon: Icon(Icons.person)),
-                  ),
-                  Padding(padding: EdgeInsets.only(top: 10.0)),
-                  TextFormField(
-                    controller: password,
-                    obscureText: !isShow,
-                    decoration: InputDecoration(
-                        labelText: "Password",
-                        prefixIcon: Icon(Icons.lock),
-                        suffixIcon: IconButton(
-                            icon: Icon(isShow
-                                ? Icons.visibility
-                                : Icons.visibility_off),
-                            onPressed: () => setShow())),
-                  ),
-                  Padding(padding: EdgeInsets.only(top: 30.0)),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 50.0,
-                    child: forcaButton(
-                        forcaText("LOGIN", color: Colors.white, fontSize: 17.0),
-                        () => login()),
-                  ),
-                  Padding(padding: EdgeInsets.only(top: 30.0)),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
                     children: <Widget>[
-                      FlatButton(
-                          onPressed: () => settingUrl(),
+                      forcaLogo(width: 130),
+                      TextFormField(
+                        controller: username,
+                        decoration: InputDecoration(
+                            labelText: "Username", prefixIcon: Icon(Icons.person)),
+                      ),
+                      Padding(padding: EdgeInsets.only(top: 10.0)),
+                      TextFormField(
+                        controller: password,
+                        obscureText: !isShow,
+                        decoration: InputDecoration(
+                            labelText: "Password",
+                            prefixIcon: Icon(Icons.lock),
+                            suffixIcon: IconButton(
+                                icon: Icon(isShow
+                                    ? Icons.visibility
+                                    : Icons.visibility_off),
+                                onPressed: () => setShow())),
+                      ),
+                      Padding(padding: EdgeInsets.only(top: 30.0)),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 50.0,
+                        child: forcaButton(
+                            forcaText("LOGIN", color: Colors.white, fontSize: 17.0),
+                            () => login()),
+                      ),
+                      Padding(padding: EdgeInsets.only(top: 30.0)),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          FlatButton(
+                              onPressed: () => settingUrl(),
 //                          onPressed: _makeError,
-                          child: forcaText("Setting",
-                              fontSize: 17.0, color: Colors.blue)),
-                      FlatButton(
-                          onPressed: _makeError,
-                          child: forcaText("How to login?",
-                              fontSize: 17.0, color: Colors.blue)),
+                              child: forcaText("Setting",
+                                  fontSize: 17.0, color: Colors.blue)),
+                          FlatButton(
+                              onPressed: _makeError,
+                              child: forcaText("How to login?",
+                                  fontSize: 17.0, color: Colors.blue)),
+                        ],
+                      )
                     ],
-                  )
+                  ),
                 ],
               ),
             ),
